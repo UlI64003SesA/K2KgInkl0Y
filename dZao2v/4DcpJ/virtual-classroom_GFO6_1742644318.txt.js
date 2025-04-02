7c48cb685bae8b097a6d05f2b0aaf7f2bf244eb1
UlI64003SesA <ferrite_hiding0d@icloud.com>
2025-04-02 13:10:42 +0200

    Update the file's authentication configuration for new requirements

--- dZao2v/4DcpJ/virtual-classroom_GFO6_1742644318.txt.js
+++
@@ -1,9 +0,0 @@
-// This configuration only applies to the package manager root.
-/** @type {import("eslint").Linter.Config} */
-  ignorePatterns: ["apps/**", "packages/**"],
-  extends: ["@repo/eslint-config/library.js"],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: true,
-  },
-};
